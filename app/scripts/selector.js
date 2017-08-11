1d69a1283ac8a3cdc801641f21ab0c1d39461967
Jose E.R <srj9es@gmail.com>
2017-08-11 09:49:32 +0200

    Abstraido el calculo de puntuación de cada parámetro

--- app/scripts/selector.js
+++ app/scripts/selector.js
@@ -27,98 +27,56 @@ app.controller("GameDevController", function ($scope, API) {
     'use strict'
     $scope.game_params = {};
 
+    var pluralize_params = {
+        'audience': 'audiences',
+        'game_system': 'game_systems',
+        'genre': 'genres',
+        'topic': 'topics'
+    }
 
-    var gameparamscore = new GameParamScore();
-    gameparamscore.pushIfNotNull(1);
+    angular.forEach(pluralize_params, function(pluralize_param, game_param){
+        API.getItems(game_param).then(function(response){
+            $scope[pluralize_param] = response.data;
+        }, function (err){
+            $scope.err = err;
+        });
+    })
 
-
-    API.getItems('audience').then(function (response) {
-        $scope.audiences = response.data;
-    }, function (err) {
-        $scope.err = err;
-    });
-
-    API.getItems('game_system').then(function (response) {
-        $scope.game_systems = response.data;
-    }, function (err) {
-        $scope.err = err;
-    });
-
-    API.getItems('genre').then(function (response) {
-        $scope.genres = response.data;
-    }, function (err) {
-        $scope.err = err;
-    });
-
-    API.getItems('topic').then(function (response) {
-        $scope.topics = response.data;
-    }, function (err) {
-        $scope.err = err;
-    });
-
-
-    $scope.getGameSystemByGenre = function (game_system, genre) {
-        game_system = game_system || $scope.game_params.game_system;
-        genre = genre || $scope.game_params.genre;
-        if (game_system && genre) {
-            return game_system.genre[genre.name.toLowerCase()];
+    $scope.getScore = function(param_key_1, param_key_2, param_val_1, param_val_2){
+        param_val_1 = param_val_1 || $scope.game_params[param_key_1];
+        param_val_2 = param_val_2 || $scope.game_params[param_key_2];
+        if(param_val_1 && param_val_2){
+            return param_val_1[param_key_2][param_val_2.name.toLowerCase()];
         }
         return null;
-    };
-
-    $scope.getTopicByGenre = function (topic, genre) {
-        topic = topic || $scope.game_params.topic;
-        genre = genre || $scope.game_params.genre;
-        if (topic && genre) {
-            return topic.genre[genre.name.toLowerCase()];
-        }
-        return null;
-    };
-
-    $scope.getGameSystemByAudience = function (game_system, audience) {
-        game_system = game_system || $scope.game_params.game_system;
-        audience = audience || $scope.game_params.audience;
-        if (game_system && audience) {
-            return game_system.audience[audience.name.toLowerCase()];
-        }
-        return null;
-    };
-
-    $scope.getTopicByAudience = function (topic, audience) {
-        topic = topic || $scope.game_params.topic;
-        audience = audience || $scope.game_params.audience;
-        if (topic && audience) {
-            return topic.audience[audience.name.toLowerCase()];
-        }
-        return null;
-    };
+    }
 
     $scope.evalAudience = function (audience) {
         var values = new GameParamScore();
-        values.pushIfNotNull($scope.getGameSystemByAudience(null, audience));
-        values.pushIfNotNull($scope.getTopicByAudience(null, audience));
+        values.pushIfNotNull($scope.getScore('game_system', 'audience', null, audience));
+        values.pushIfNotNull($scope.getScore('topic', 'audience', null, audience));
         return values.getMin();
     };
 
     $scope.evalGameSystem = function (game_system) {
         var values = new GameParamScore();
-        values.pushIfNotNull($scope.getGameSystemByAudience(game_system, null));
-        values.pushIfNotNull($scope.getGameSystemByGenre(game_system, null));
+        values.pushIfNotNull($scope.getScore('game_system', 'audience', game_system, null));
+        values.pushIfNotNull($scope.getScore('game_system', 'genre', game_system, null));
         return values.getMin();
     };
 
     $scope.evalGenre = function (genre) {
         var values = new GameParamScore();
-        values.pushIfNotNull($scope.getTopicByGenre(null, genre));
-        values.pushIfNotNull($scope.getGameSystemByGenre(null, genre));
+        values.pushIfNotNull($scope.getScore('topic', 'genre', null, genre));
+        values.pushIfNotNull($scope.getScore('game_system', 'genre', null, genre));
         return values.getMin();
     };
 
 
     $scope.evalTopic = function (topic) {
         var values = new GameParamScore();
-        values.pushIfNotNull($scope.getTopicByGenre(topic, null));
-        values.pushIfNotNull($scope.getTopicByAudience(topic, null));
+        values.pushIfNotNull($scope.getScore('topic', 'genre', topic, null));
+        values.pushIfNotNull($scope.getScore('topic', 'audience', topic, null));
         return values.getMin();
     };
 
